7c87660f13629fa3fb6cecc51d6d2733682f6cae
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-03 14:38:01 +0900

    I like it like that
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -2,62 +2,39 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# begin properties
-properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
-do.modules=0
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
-device.name5=
-supported.versions=
-'; } # end properties
-
-# shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
-
+eval $(cat /tmp/anykernel/props | grep -v '\.')
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
-
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
+# Change skip_initramfs to want_initramfs if Magisk is detected
+IMAGE=/tmp/anykernel/Image;
+if [ ! -e $IMAGE ]; then
+  gzip -dc < ${IMAGE}.gz > $IMAGE;
+fi
+if [ -d $ramdisk/.backup ]; then
+  ui_print " ";
+  ui_print "Magisk detected!";
+  ui_print "Patching kernel so that reflashing Magisk is not necessary...";
+  sed -i -e 's/skip_initramfs/want_initramfs/g' $IMAGE;
+  gzip -9 < $IMAGE > ${IMAGE}.gz;
+fi
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+if [ ! -e ${IMAGE}.gz ]; then
+  gzip -9 < $IMAGE > ${IMAGE}.gz;
+fi
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+cat ${IMAGE}.gz /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+rm $IMAGE ${IMAGE}.gz;
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
+# Install the boot image
 write_boot;
-
-## end install
-
