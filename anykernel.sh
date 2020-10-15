5231aed8965edb80994daeba170c624149d29133
Park Ju Hyung <qkrwngud825@gmail.com>
2020-10-16 00:28:06 +0900

    Install at the inactive slot too
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -16,15 +16,31 @@ eval $(cat $home/props | grep -v '\.')
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## AnyKernel install
-dump_boot;
+install() {
+  ## AnyKernel install
+  dump_boot;
 
-# Use the provided dtb
-if [ -e $home/dtb ]; then
-  mv $home/dtb $home/split_img/;
-fi
+  # Use the provided dtb
+  if [ -e $home/dtb ]; then
+    mv $home/dtb $home/split_img/;
+  fi
 
-# Install the boot image
-write_boot;
+  # Install the boot image
+  write_boot;
+}
+
+install;
+
+case $is_slot_device in
+  1|auto)
+    ui_print " ";
+    ui_print "Installing to the secondary slot";
+    slot_select=inactive;
+    unset block;
+    eval $(cat $home/props | grep '^block=' | grep -v '\.')
+    reset_ak;
+    install;
+  ;;
+esac
 
 ## end install
